28c32075c715f9c33983e762c7319dc35f572d68
onomatopoetica <hi.jen@icloud.com>
2022-05-20 13:19:01 -0700

    created link_to post for blog posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,31 @@
 class PostsController < ApplicationController
 
   def index
+    @posts = Post.all.order("created_at DESC")
   end
 
   def new
+    @post = Post.new
   end
 
   def create
-  end
-end
+    @post = Post.new(post_params)
 
+    if @post.save
+      redirect_to @post
+    else
+      render "new"
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
+end
