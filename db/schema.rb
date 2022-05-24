4ed6c246563bb9d3e3b838d8b59dd47963a2a373
onomatopoetica <hi.jen@icloud.com>
2022-05-24 11:10:50 -0700

    Added comments section + fixed error on deleting comment with redirect_to root_path

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_20_165705) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_23_212507) do
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
